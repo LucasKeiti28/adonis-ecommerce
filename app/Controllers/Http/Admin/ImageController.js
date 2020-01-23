dbd1016d93ed64afac6660c30a5c44da4fc7a64d
LucasKeiti28 <lucaskeiti28@gmail.com>
2020-01-23 18:55:11 -0300

    Criacao do metodo store para single and multiple images.

--- app/Controllers/Http/Admin/ImageController.js
+++ app/Controllers/Http/Admin/ImageController.js
@@ -4,6 +4,9 @@
 /** @typedef {import('@adonisjs/framework/src/Response')} Response */
 /** @typedef {import('@adonisjs/framework/src/View')} View */
 
+const Image = use('App/Models/Image')
+const { manage_single_upload, manage_multiple_uploads } = use('App/Helpers')
+
 /**
  * Resourceful controller for interacting with images
  */
@@ -17,19 +20,11 @@ class ImageController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async index ({ request, response, view }) {
-  }
-
-  /**
-   * Render a form to be used for creating a new image.
-   * GET images/create
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async create ({ request, response, view }) {
+  async index({ response, pagination }) {
+    const images = await Image.query()
+      .orderBy('id', 'DESC')
+      .paginate(pagination.page, pagination.limit)
+    return response.send(images)
   }
 
   /**
@@ -40,7 +35,58 @@ class ImageController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async store ({ request, response }) {
+  async store({ request, response }) {
+    try {
+      // Captura uma ou mais imagens do request
+      const fileJar = request.file('images', {
+        types: ['image'],
+        size: '2mb'
+      })
+
+      let images = []
+
+      // Caso seja um unico file - manage_single_upload
+
+      if (!fileJar.files) {
+        const file = await manage_single_upload(fileJar)
+        if (file.moved()) {
+          const image = await Image.create({
+            path: file.fileName,
+            size: file.size,
+            original_name: file.clientName,
+            extension: file.subtype
+          })
+          images.push(image)
+
+          return response.status(201).send({ successes: images, errors: {} })
+        }
+        return response
+          .status(400)
+          .send({ message: 'nao foi possivel processar a imagem no momento' })
+      }
+      // Caso seja multiplos files - manage_multiple_uploads
+
+      let files = await manage_multiple_uploads(fileJar)
+
+      await Promise.all(
+        file.successes.map(async file => {
+          const image = Image.create({
+            path: file.fileName,
+            size: file.size,
+            original_name: file.clientName,
+            extension: file.subtype
+          })
+          images.push(image)
+        })
+      )
+      return response
+        .status(201)
+        .send({ successes: images, errors: files.errors })
+    } catch (error) {
+      return response
+        .status(400)
+        .send({ message: 'Nao foi possivel processar a sua solicitacao.' })
+    }
   }
 
   /**
@@ -52,20 +98,7 @@ class ImageController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async show ({ params, request, response, view }) {
-  }
-
-  /**
-   * Render a form to update an existing image.
-   * GET images/:id/edit
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async edit ({ params, request, response, view }) {
-  }
+  async show({ params, request, response, view }) {}
 
   /**
    * Update image details.
@@ -75,8 +108,7 @@ class ImageController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async update ({ params, request, response }) {
-  }
+  async update({ params, request, response }) {}
 
   /**
    * Delete a image with id.
@@ -86,8 +118,7 @@ class ImageController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async destroy ({ params, request, response }) {
-  }
+  async destroy({ params, request, response }) {}
 }
 
 module.exports = ImageController
