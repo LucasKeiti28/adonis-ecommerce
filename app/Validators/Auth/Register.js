46e6f589e85794227b5c33c2d488c7458ce93738
LucasKeiti28 <lucaskeiti28@gmail.com>
2020-01-24 17:59:31 -0300

    Retornando erros da validacao.

--- app/Validators/Auth/Register.js
+++ app/Validators/Auth/Register.js
@@ -9,6 +9,18 @@ class AuthRegister {
       password: 'required/confirmed'
     }
   }
+
+  get messages() {
+    return {
+      'name.required': 'O nome é obrigatorio',
+      'surname.required': 'O sobrenome e obrigatorio',
+      'email.required': 'O email eh obrigatorio',
+      'email.email': 'O email eh invalido',
+      'email.unique': 'Este email ja existe',
+      'password.required': 'A senha eh obrigatoria',
+      'password.confirmed': 'As senhas nao sao iguais'
+    }
+  }
 }
 
 module.exports = AuthRegister
