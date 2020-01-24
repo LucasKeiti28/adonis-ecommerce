858897c182b6146358dd472469cd5eb7821aa232
LucasKeiti28 <lucaskeiti28@gmail.com>
2020-01-24 10:25:44 -0300

    Editar cupom com multiplas relacoes (3).

--- app/Controllers/Http/Admin/CouponController.js
+++ app/Controllers/Http/Admin/CouponController.js
@@ -132,7 +132,63 @@ class CouponController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async update({ params, request, response }) {}
+  async update({ params: { id }, request, response }) {
+    const trx = await Database.beginTransaction()
+    const coupon = await Coupon.findOrFail(id)
+
+    var can_use_for = {
+      product: false,
+      client: false
+    }
+
+    try {
+      const couponData = request.only([
+        'code',
+        'discount',
+        'valid_from',
+        'valid_until',
+        'quantity',
+        'type',
+        'recursive'
+      ])
+
+      coupon.merge(couponData)
+
+      const { users, products } = request.only(['users', 'products'])
+
+      const service = new Service(coupon, trx)
+
+      if (users && users.length > 0) {
+        await service.syncUsers(users)
+        can_use_for.client = true
+      }
+
+      if (products && products.length > 0) {
+        await service.syncProducts(products)
+        can_use_for.product = true
+      }
+
+      if (can_use_for.client && can_use_for.product) {
+        coupon.can_use_for = 'product_client'
+      } else if (!can_use_for.client && can_use_for.product) {
+        coupon.can_use_for = 'product'
+      } else if (can_use_for.client && !can_use_for.product) {
+        coupon.can_use_for = 'client'
+      } else {
+        coupon.can_use_for = 'all'
+      }
+
+      await coupon.save(trx)
+      await trx.commit()
+
+      return response.status(200).send(coupon)
+    } catch (error) {
+      await trx.rollback()
+      return response
+        .status(401)
+        .send({ message: 'Cupom nao cadastrado, tente novamente.' })
+    }
+  }
 
   /**
    * Delete a coupon with id.
