a6a496011db176f09b95060aed6ebdc6fe28d900
LucasKeiti28 <lucaskeiti28@gmail.com>
2020-01-25 10:40:20 -0300

    Criando o broadcast para User.store.

--- app/Controllers/Http/Auth/AuthController.js
+++ app/Controllers/Http/Auth/AuthController.js
@@ -3,6 +3,7 @@
 const Database = use('Database')
 const User = use('App/Models/User')
 const Role = use('Role')
+const Ws = use('Ws')
 
 class AuthController {
   async register({ request, response }) {
@@ -16,6 +17,11 @@ class AuthController {
       await user.roles().attach([userRole.id], null, trx)
 
       await trx.commit()
+
+      const topic = Ws.getChannel('notifications').topic('notifications')
+      if (topic) {
+        topic.broadcast('new:user')
+      }
 
       return response.status(201).send({ data: user })
     } catch (error) {
